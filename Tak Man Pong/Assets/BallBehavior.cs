2abf4070e5fdf28c7c423a8fa11739a61cc384ff
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-07-12 14:27:34 +0200

    add project

---
+++ Tak Man Pong/Assets/BallBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    public Vector3 ballDirection;
+    public bool goingRight;
+    public float ballSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((int)Random.Range(0, 2) == 1)
+        {
+            goingRight = true;
+        } else
+        {
+            goingRight = false;
+        }
+        ballDirection = new Vector3(1, Random.Range(-1f, 1f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goingRight)
+        {
+            //gameObject.transform.position += new Vector3(ballSpeed, 0, 0);
+            gameObject.transform.position += ballDirection * ballSpeed;
+        } else
+        {
+            //gameObject.transform.position += new Vector3(-ballSpeed, 0, 0);
+            gameObject.transform.position += ballDirection * -ballSpeed;
+        }
+    }
+}
---
+++ Tak Man Pong/Assets/PaddleBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehavior : MonoBehaviour
+{
+    public int score;
+    public float moveSpeed;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float maxHeight;
+    public float minHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        maxHeight = 5.5f;
+        minHeight = -3.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveUp) && gameObject.transform.position.y <= maxHeight)
+        {
+            gameObject.transform.position += new Vector3(0, moveSpeed, 0);
+        }
+        if (Input.GetKey(moveDown) && gameObject.transform.position.y >= minHeight)
+        {
+            gameObject.transform.position -= new Vector3(0, moveSpeed, 0);
+        }
+    }
+}
